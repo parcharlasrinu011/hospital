a8ee0bdedced2ebc05426dd98ad6b3cecef14092
parcharlasrinu011 <parcharlasrinu011@gmail.com>
2023-12-13 11:20:01 +0530

    first commit

---
+++ management/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from management.views import base,doctor,appointment,dental,physiotherapist,nuerology,gallery,home
+
+
+
+
+
+app_name = 'management'
+
+urlpatterns=[
+    path('',base,name='base'),
+    path('doctor/',doctor,name='doctor'),
+    path('appointment/',appointment,name='appointment'),
+    path('dental/',dental,name='dental'),
+    path('physiotherapist/',physiotherapist,name='physiotherapist'),
+    path('nuerology/',nuerology,name='nuerology'),
+    path('gallery/',gallery,name='gallery'),
+    path('home/',home,name='home'),
+    
+    
+]
